26dc39d702fedcab928faa9683751d17eaf5a754
lgs2007m <116912542@qq.com>
2026-05-01 09:42:42 +0800

    Use the default source for easytier

--- YVR-X6-hanwckf/diy-part1.sh
+++ YVR-X6-hanwckf/diy-part1.sh
@@ -17,5 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist2
-git clone https://github.com/lgs2007m/luci-app-easytier package/easytier
+git clone https://github.com/EasyTier/luci-app-easytier package/easytier
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
